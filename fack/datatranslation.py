dec4fcf7af7807c6427c93e7f4bcc8831227590f
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-09-19 16:08:15 +0200

    extra comment

--- fack/datatranslation.py
+++ fack/datatranslation.py
@@ -2,6 +2,11 @@ from datatrans.utils import register
 
 from fack.models import Topic, Question
 
+##
+## Works for django-datatrans==0.1.5
+## https://github.com/vikingco/django-datatrans
+##
+
 
 class TopicTranslation(object):
     fields = ('name',)
